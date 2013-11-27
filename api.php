fefbc1bae7844644217091b7b5de87489a119d6f
Hiroyuki Kato <dev.kato@gmail.com>
2013-11-27 12:24:32 +0900

    add content-type def

--- api.php
+++ api.php
@@ -1,8 +1,11 @@
 <?php
+  header("Content-Type: application/json; charset=utf-8");
+
   $arr = array(
     'path'         =>  $_SERVER['REQUEST_URI'],
     'ip_address'   =>  $_SERVER['REMOTE_ADDR'],
     'user_agent'   =>  $_SERVER['HTTP_USER_AGENT'],
+    'japanese'     =>  'リラックマ',
     'timestamp'    =>  time()
   );
 
