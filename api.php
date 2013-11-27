ea68319f1b8e364697d3cde250e7665764713530
Hiroyuki Kato <dev.kato@gmail.com>
2013-11-27 16:45:38 +0900

    rename

--- api.php
+++ api.php
@@ -1,16 +1,22 @@
 <?php
+
   header("Content-Type: application/json; charset=utf-8");
 
-  $arr = array(
-    'path'         =>  $_SERVER['REQUEST_URI'],
-    'ip_address'   =>  $_SERVER['REMOTE_ADDR'],
-    'user_agent'   =>  $_SERVER['HTTP_USER_AGENT'],
-    'parent'       =>  array(
-      'japanese' => 'リラックマ',
-      'array'    => array('element1', 'element2', 'element3')
-    ),
-    'timestamp'    =>  time()
-  );
+  $response = array();
 
-  echo json_encode($arr);
+  switch ($_SERVER['REQUEST_URI']) {
+    case '/api/hello':
+      $response['data'] = 'world';
+      break;
+    case '/api/item':
+      $response['data'] = array(
+        'id'          => 100,
+        'title'       => 'やばい商品',
+        'price'       => 5000,
+        'description' => 'これはまじでやばい'
+      );
+      break;
+  }
+
+  echo json_encode($response);
 ?>
