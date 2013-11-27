7f751207b44772a0a2d8ef21eea7e66f539e9970
Hiroyuki Kato <dev.kato@gmail.com>
2013-11-27 16:51:08 +0900

    hoge

---
+++ hello_api.php
@@ -0,0 +1,16 @@
+<?php
+  header("Content-Type: application/json; charset=utf-8");
+
+  $arr = array(
+    'path'         =>  $_SERVER['REQUEST_URI'],
+    'ip_address'   =>  $_SERVER['REMOTE_ADDR'],
+    'user_agent'   =>  $_SERVER['HTTP_USER_AGENT'],
+    'parent'       =>  array(
+      'japanese' => 'リラックマ',
+      'array'    => array('element1', 'element2', 'element3')
+    ),
+    'timestamp'    =>  time()
+  );
+
+  echo json_encode($arr);
+?>
